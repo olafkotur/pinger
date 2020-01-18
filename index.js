f0beebf4868509a9beb19a9d0402ffa873c6c748
olafkotur <olafkotur97@gmail.com>
2020-01-18 20:19:20 +0000

    Testing CI with netlify

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const Helper = require('./helper');
 
 const options = {
   uri: process.env.URI ? process.env.URI : 'https://localhost:8080',
-  minTimeout: 300000,
-  maxTimeout: 1200000,
+  // minTimeout: 300000,
+  // maxTimeout: 1200000,
+  minTimeout: 1000,
+  maxTimeout: 10000,
 }
 
 async function main() {
